333e6e656ef60695fbd87cdd4aa2498d072c368e
Nurdaulet <nurik93sat@gmail.com>
2025-09-10 17:59:19 +0500

    Created User TypeOrm Entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'users'
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false
+  })
+  firstName: string
+
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false
+  })
+  lastName: string
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 96,
+    nullable: false
+  })
+  password: string;
+}
