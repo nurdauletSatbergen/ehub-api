6565cac534dae936e1e439cba1f7ab463c0aaef8
Nurdaulet <nurik93sat@gmail.com>
2025-09-11 05:10:12 +0500

    Added auth config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,24 +7,31 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import jwtConfig from './config/jwt.config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   providers: [
     AuthService,
     LocalStrategy,
-    JwtStrategy
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
   controllers: [AuthController],
   imports: [
+    ConfigModule.forFeature(jwtConfig),
     UsersModule,
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      //TODO create a config file for auth and put secret
       useFactory: (configService: ConfigService) => ({
         global: true,
-        secret: 'secret',
+        secret: configService.get<string>('jwt.secret'),
         signOptions: {
           expiresIn: '3600s'
         }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { User } from '../../users/entities/user.entity';
+import jwtConfig from '../config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    @Inject(jwtConfig.KEY) private readonly config: ConfigType<typeof jwtConfig>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret'
+      secretOrKey: config.secret
     });
   }
 
-  async validate(payload: Omit<User, "password">) {
+  async validate(payload: Omit<User, 'password'>) {
     return { ...payload };
   }
 }
