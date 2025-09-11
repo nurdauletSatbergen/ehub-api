0774e6d2af8ee7f0074ea729f4e46cf21c5b7c34
Nurdaulet <nurik93sat@gmail.com>
2025-09-11 05:17:35 +0500

    Removed jwt guard from controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,13 @@ import {
   Controller,
   Post,
   UseGuards,
-  Request,
   Get,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from '../users/entities/user.entity';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -17,13 +16,14 @@ export class AuthController {
     private authService: AuthService
   ) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('sign-in')
-  signIn(@Request() req) {
-    return this.authService.login(req.user);
+  signIn(@GetUser() user: Omit<User, "password">) {
+    return this.authService.login(user);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard) we use global auth guard with JWT in auth.guard.ts that is why anymore need this
   @Get('profile')
   getProfile(@GetUser() user: Omit<User, "password">) {
     return user;
